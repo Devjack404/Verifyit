6b77c3c8fb31eb8284701adf48ed4a82ae05842e
Devjack404 <juansabarofek@gmail.com>
2026-07-14 22:48:57 +0700

    feat: add Navbar components including Logo, NavLinks, NavActions, and navigation constants

---
+++ VerifyIt/src/apps/frontend/components/Navbar/NavbarAction.tsx
@@ -0,0 +1,5 @@
+export default function NavActions(){
+    return (
+        <button> Analyze </button>
+    );
+}
---
+++ VerifyIt/src/apps/frontend/constants/navigation.tsx
@@ -0,0 +1,14 @@
+export const NAVIGATION = [
+  {
+    title: "Features",
+    href: "/features",
+  },
+  {
+    title: "How It Works",
+    href: "/how-it-works",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+];
