dbbeb64080543c44ff2ffd95a96caaf8ddfb23d8
Devjack404 <juansabarofek@gmail.com>
2026-07-16 22:21:01 +0700

    feat : add analyzeButton in searchbox

---
+++ VerifyIt/src/apps/frontend/components/SearchBar/AnalyzeButton.tsx
@@ -0,0 +1,5 @@
+export default function AnalyzeButton (){
+    return (
+        <button>Analyze</button>
+    );
+}
--- VerifyIt/src/apps/frontend/components/SearchBar/SearchBar.tsx
+++ VerifyIt/src/apps/frontend/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,11 @@
 import SearchInput from "./SearchInput";
+import AnalyzeButton from "./AnalyzeButton";
 
 export default function SearchBar (){
     return (
         <>
             <SearchInput /> 
-        
+            <AnalyzeButton />
         </>
     );
 }
