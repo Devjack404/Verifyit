bb6f08d1bd1235dda7f929e1dfdbfe7f2d9a47c8
Devjack404 <juansabarofek@gmail.com>
2026-07-16 22:24:15 +0700

    feat: add ClearButton component to SearchBar

---
+++ VerifyIt/src/apps/frontend/components/SearchBar/ClearButton.tsx
@@ -0,0 +1,5 @@
+export default function ClearButton () {
+    return (
+        <button>Clear</button>
+    );
+}
--- VerifyIt/src/apps/frontend/components/SearchBar/SearchBar.tsx
+++ VerifyIt/src/apps/frontend/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,12 @@
 import SearchInput from "./SearchInput";
 import AnalyzeButton from "./AnalyzeButton";
+import ClearButton from "./ClearButton";
 
 export default function SearchBar (){
     return (
         <>
             <SearchInput /> 
+            <ClearButton />
             <AnalyzeButton />
         </>
     );
