546aaceeeeb9385e4a944d9cea3b0294e29e0cdc
Devjack404 <juansabarofek@gmail.com>
2026-07-23 00:31:34 +0700

    fix: update TypeScript version to 7.0.2 and add spacing in App component

--- VerifyIt/src/apps/frontend/App.tsx
+++ VerifyIt/src/apps/frontend/App.tsx
@@ -2,6 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import AnalyzePage from "./pages/AnalyzePage";
 
+
 export default function MyApp() {
   return (
     <div className="flex flex-1 flex-col bg-gray-100 min-h-screen ">
