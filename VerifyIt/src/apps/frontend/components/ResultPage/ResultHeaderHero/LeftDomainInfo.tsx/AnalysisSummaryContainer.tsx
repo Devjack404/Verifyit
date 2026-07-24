9bb9dff732c3bea693f8546be3b77042850a1f75
Devjack404 <juansabarofek@gmail.com>
2026-07-24 23:14:33 +0700

    feat: refactor ResultPage by adding new DomainAnalysisContainer and AnalysisSummaryContainer components

---
+++ VerifyIt/src/apps/frontend/components/ResultPage/ResultHeaderHero/LeftDomainInfo.tsx/AnalysisSummaryContainer.tsx
@@ -0,0 +1,10 @@
+export default function AnalysisSummarryContainer (){
+    return (
+        <div className="bg-gray-50">
+            <h2>example-secure-shop.com</h2>
+            <p>Analysis Date :</p>
+            <button>Share</button>            
+            <button>Copy Link</button>            
+        </div>
+    )
+}
--- VerifyIt/src/apps/frontend/components/ResultPage/ResultHeaderHero/ResultPage.tsx
+++ VerifyIt/src/apps/frontend/components/ResultPage/ResultHeaderHero/ResultPage.tsx
@@ -1,9 +1,20 @@
-import DomainAnalysisCon from "./LeftDomainInfo.tsx/DomainAnalysisCon";
+import DomainAnalysisCon from "./LeftDomainInfo.tsx/DomainAnalysisContainer";
+import AnalysisSummarryContainer from "./LeftDomainInfo.tsx/AnalysisSummaryContainer";
 
 export default function ResultPage (){
     return (
-        <div className="flex flex-2 flex-col items-center px-6 pt-20">
-            <DomainAnalysisCon />
-        </div>
+        <section className="mx-auto flex max-w-7xl gap-8 px-6 pt-20">
+
+            {/* left container  */}
+            <div className="">
+                <DomainAnalysisCon />
+                <AnalysisSummarryContainer />
+            </div>
+
+            {/* Right Container */}
+            <div>
+                
+            </div>
+        </section>
     );
 }
