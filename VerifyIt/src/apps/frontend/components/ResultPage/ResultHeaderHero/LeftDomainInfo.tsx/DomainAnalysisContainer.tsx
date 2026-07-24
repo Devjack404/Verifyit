904b4fa5371136f99f203ff37339332de17e5faf
Devjack404 <juansabarofek@gmail.com>
2026-07-24 23:59:15 +0700

    feat: enhance DomainAnalysisContainer layout with improved styling and structure

--- VerifyIt/src/apps/frontend/components/ResultPage/ResultHeaderHero/LeftDomainInfo.tsx/DomainAnalysisContainer.tsx
+++ VerifyIt/src/apps/frontend/components/ResultPage/ResultHeaderHero/LeftDomainInfo.tsx/DomainAnalysisContainer.tsx
@@ -1,10 +1,13 @@
 export default function DomainAnalysisContainer (){
     return (
         <div className="bg-gray-50 ">
-            <h2>example-secure-shop.com</h2>
+            <h2 className="text-2xl">example-secure-shop.com</h2>
             <p>Analysis Date :</p>
-            <button>Share</button>            
-            <button>Copy Link</button>            
+            <div>
+                <button>Share</button>            
+                <button>Copy Link</button>
+            </div>
+            
         </div>
     )
 }
