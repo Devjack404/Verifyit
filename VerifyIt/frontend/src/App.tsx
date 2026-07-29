0a9794a5e676dc9b674739abb38e2857fb2f3724
Devjack404 <juansabarofek@gmail.com>
2026-07-29 15:17:39 +0700

    fix: update file paths in index.html and tsconfig.app.json for correct resource loading; rename MyApp to App in App.tsx

--- VerifyIt/frontend/src/App.tsx
+++ VerifyIt/frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import AnalyzePage from "./pages/AnalyzePage";
 import DisplayResultPage from "./pages/DisplayResultPage";
 
-export default function MyApp() {
+export default function App() {
   return (
     <div className="flex flex-1 flex-col bg-gray-100 min-h-screen ">
       <Navbar />
