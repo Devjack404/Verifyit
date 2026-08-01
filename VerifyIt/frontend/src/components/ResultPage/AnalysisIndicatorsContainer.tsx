02c0b4d8dd147626342c525cdd263a936e15952b
Devjack404 <juansabarofek@gmail.com>
2026-08-01 16:19:28 +0700

    fix: adjust layout of AnalysisIndicatorsContainer and AreaAwernessSubCard for improved structure and consistency

--- VerifyIt/frontend/src/components/ResultPage/AnalysisIndicatorsContainer.tsx
+++ VerifyIt/frontend/src/components/ResultPage/AnalysisIndicatorsContainer.tsx
@@ -3,9 +3,14 @@ import AreaAwernessCard from "./AreaAwernessCard";
 
 export default function AnalysisIndicatorsContainer () {
     return (
-        <div className="flex w-full mb-30 mt-5 gap-8">
+    <div className="flex w-full mb-30 mt-5 gap-8">
+        <div className="flex-1">
             <PositiveFactorsCard />
+        </div>
+
+        <div className="flex-1">
             <AreaAwernessCard />
         </div>
+    </div>
     );
 }
