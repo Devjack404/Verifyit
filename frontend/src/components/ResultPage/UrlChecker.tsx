f8ca83257690ab2d0db61b82078d9f0eba0f7b1d
Devjack404 <juansabarofek@gmail.com>
2026-08-04 22:47:04 +0700

    style: improve layout and spacing in UrlChecker component

--- frontend/src/components/ResultPage/UrlChecker.tsx
+++ frontend/src/components/ResultPage/UrlChecker.tsx
@@ -2,17 +2,15 @@ import SearchBar from "../SearchBar/SearchBar";
 
 export default function UrlChecker (){
     return (
-        <div className="flex w-full bg-gray-200 p-10 rounded-4xl justify-between ">
-            <div>
+        <div className="flex w-full items-center bg-gray-200 p-10 rounded-4xl justify-between ">
+            <div className="space-y-2">
                 <h2 className="font-bold text-2xl">Check Another URL</h2>
-                <p>Analyze another site for instant security insights.</p>
+                <p className="mt-2 text-gray-600">Analyze another site for instant security insights.</p>
             </div>
 
-            <div className="">
+            <div className="w-full lg:w-[750px]">
                 <SearchBar />
             </div>
-
-        
         </div>
     );
 }
