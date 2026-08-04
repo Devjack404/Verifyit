f80a179dcc25086602df852fed366cbb98e6730b
Devjack404 <juansabarofek@gmail.com>
2026-08-04 23:32:30 +0700

    refactor: update AboutSecurityCard layout and restructure AboutSecurityContainer for better grid display

--- frontend/src/components/ResultPage/AboutSecurityCard.tsx
+++ frontend/src/components/ResultPage/AboutSecurityCard.tsx
@@ -1,6 +1,6 @@
 export default function AboutSecurityCard(){
     return (
-        <div className="flex-col h-full flex space-y-4 shadow-md  bg-white p-10 rounded-2xl mb-20">
+        <div className="flex-col h-full flex space-y-4 shadow-md  bg-white p-10 rounded-2xl">
             <h4 className="font-bold">What is SSL</h4>
             <p>Understand how Secure Sockets Layer keeps your connection encrypted and data private.</p>
 
--- frontend/src/components/ResultPage/AboutSecurityContainer.tsx
+++ frontend/src/components/ResultPage/AboutSecurityContainer.tsx
@@ -2,10 +2,18 @@ import AboutSecurityCard from "./AboutSecurityCard"
 
 export default function AboutSecurityContainer() {
     return (
-        <div className="flex w-full  justify-between gap-10 items-center">
-            <AboutSecurityCard />        
-            <AboutSecurityCard />        
-            <AboutSecurityCard />        
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 w-full gap-10 mb-20">
+            <div className="flex-1">
+                <AboutSecurityCard />   
+            </div>     
+
+            <div className="flex-1">
+                <AboutSecurityCard />        
+            </div>
+
+            <div className="flex-1">
+                <AboutSecurityCard />        
+            </div>         
         </div>
     )
 }
