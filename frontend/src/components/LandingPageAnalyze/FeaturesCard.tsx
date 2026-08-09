7a42c076b0156bcf08a2a186d678a1bfae301b51
Devjack404 <juansabarofek@gmail.com>
2026-08-09 16:57:47 +0700

    feat: implement FeaturesCard component and integrate it into FeaturesSectionContainer

---
+++ frontend/src/components/LandingPageAnalyze/FeaturesCard.tsx
@@ -0,0 +1,29 @@
+import type { Feature } from "./features.data";
+
+interface FeatureCardProps {
+    feature : Feature;
+}
+
+export default function FeaturesCard({
+    feature,
+}: FeatureCardProps) {
+    return (
+        <article 
+            className={`rounded-2xl border border-gray-200 bg-wehite p-6 ${feature.gridClassName}`}>
+
+            <div 
+                className={`mb-4 flex h-10 w-10 items-center justify-center rounded-xl ${feature.iconClassName}`}
+            >
+                {feature.icon}
+            </div>
+
+            <h3 className={`mb-3 text-xl font-semibold`}>
+                {feature.title}
+            </h3>
+
+            <p className={`text-sm leading-6 text-gray-600`}>
+                {feature.description}
+            </p>
+        </article>
+    );
+}
--- frontend/src/components/LandingPageAnalyze/FeaturesSectionContainer.tsx
+++ frontend/src/components/LandingPageAnalyze/FeaturesSectionContainer.tsx
@@ -1,8 +1,31 @@
+import { features } from "./features.data";
+import FeaturesCard from "./FeaturesCard";
+
 export default function FeaturesSectionContainer () {
     return (
-        <div>
-            
+        <section className="bg-[#f8f8ff] py-20">
+            <div className="mx-auto max-w-6xl px-6">
 
-        </div>
-    )
+                <div className="mb-14 text-center">
+                    <span className="text-xs font-bold tracking-widest text-blue-600">
+                        CAPABILITIES
+                    </span>
+
+                    <h2 className="mt-3 text-3xl font-bold">
+                        Powerful analysis at your fingertips
+                    </h2>
+                </div>
+
+                <div className="grid grid-cols-6 gap-5">
+                    {features.map((feature) => (
+                        <FeaturesCard 
+                            key={feature.title}
+                            feature={feature}
+                        />
+                    ))}
+                </div>
+
+            </div>
+        </section>
+   )
 }
