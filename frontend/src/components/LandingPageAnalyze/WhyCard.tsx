f4118ccfab74fe423b49a878f606b060e2b996c3
Devjack404 <juansabarofek@gmail.com>
2026-08-12 23:30:45 +0700

    feat: update WhyCard component structure with section and improved layout
    add example image

--- frontend/src/components/LandingPageAnalyze/WhyCard.tsx
+++ frontend/src/components/LandingPageAnalyze/WhyCard.tsx
@@ -1,8 +1,24 @@
 export default function WhyCard () {
     return (
-        <>
+        <section className="py-16 px-6 flex">
+            <div className="flex-col">
+                <h2>Built for the modern web, designed for humans</h2>
+
+                
+            </div>
+
+            <div>
+                <img 
+                    src="https://i.pinimg.com/1200x/81/5e/06/815e06f77247d1c8fc341ba3b8552cfc.jpg" 
+                    alt="Web Analyze Ilustration"
+                    className="w-full rounded-2xl"
+                >
+                </img>  
+            </div>
+
+
         
-        </>
+        </section>
     )
     
 }
