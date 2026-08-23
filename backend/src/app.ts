62af477254219f3cfad39f59da710c8a5485a5b8
Devjack404 <juansabarofek@gmail.com>
2026-08-23 23:13:18 +0700

    feat: add CORS support and update root route response in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express, {type Express, type Request, type Response} from 'express';
+import cors from "cors";
 
 const app : Express = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req: request, res: Response) => {
-    res.send('Hello World')
+app.get('/', (req: Request, res: Response) => {
+    res.send()
 });
 
 app.listen(3000, () => {
