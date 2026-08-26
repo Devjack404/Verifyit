500789e41b7f3535c089b3a849799160e18bbae9
Devjack404 <juansabarofek@gmail.com>
2026-08-26 19:43:21 +0700

    feat: update analyze route to handle POST requests and return URL in response; remove unused click handler from AnalyzeButton component

--- backend/src/routes/analyze.routes.ts
+++ backend/src/routes/analyze.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import type { Request, Response } from "express";
 
 const routerAnalyze = Router();
 
-routerAnalyze.post("/analyze"), 
+routerAnalyze.post("/analyze", (req, res) => {
+    const url = req.body.url;
+    console.log(url)
+
+    res.json({
+        url : url
+    })
+});
 
 export default routerAnalyze;
--- frontend/src/components/SearchBar/AnalyzeButton.tsx
+++ frontend/src/components/SearchBar/AnalyzeButton.tsx
@@ -1,14 +1,9 @@
 import SearchInput from "./SearchInput";
 
 export default function AnalyzeButton (){
-    const handleClick = () => {
-        console.log(SearchInput)
-    }
-
     return (
         <button 
             className="bg-blue-800 py-3 px-8 rounded-lg text-white"
-            onClick={handleClick}
         >
             Analyze
         </button>
