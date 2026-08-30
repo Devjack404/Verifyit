77b987310b4f4cd210e510931d6210d61f7a197d
Devjack404 <juansabarofek@gmail.com>
2026-08-30 22:56:44 +0700

    feat: add CheckUseIP function to validate URL against IP addresses and create Envidence type definition

---
+++ backend/src/rules/rules.ts
@@ -0,0 +1,16 @@
+function CheckUseIP(urlText : string) {
+    const urlObject = new URL(urlText);
+    const domain = urlObject.hostname;
+
+    const splitDomain = domain.split(".");
+
+    const isFourPart = splitDomain.length === 4;
+    const isAllNumbers = splitDomain.every(item => !isNaN(Number(item)));
+
+    if(isFourPart && isAllNumbers) {
+        return "Peringatan : URL ini menggunakana Alamat IP, bukan domain resmi !"
+    }
+
+    return "Aman : URL menggunakan domain biasa"
+
+}
