32c0d926d5d1f7178971e3427edf5b34fd09292a
Devjack404 <juansabarofek@gmail.com>
2026-09-13 21:03:04 +0700

    feat: integrate React Query and Devtools for improved data fetching and state management using TanStack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,35 @@ import Navbar from "./components/Navbar/Navbar";
 import AnalyzePage from "./pages/AnalyzePage";
 import DisplayResultPage from "./pages/DisplayResultPage";
 import LandingPageAnalyze from "./pages/LandingPageAnalyze";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools' 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div className="flex flex-1 flex-col bg-gray-100 min-h-screen ">
-      <Navbar />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <div className="flex flex-1 flex-col bg-gray-100 min-h-screen ">
+        <Navbar />
 
-      <main className="flex-1 ">
-        <Routes>
-          <Route path="/analyze" element={<AnalyzePage />} />
-          <Route path="/result" element={<DisplayResultPage />} />
-          <Route path="/" element={<LandingPageAnalyze />} />
-        </Routes>
-      </main>
+        <main className="flex-1 ">
+          <Routes>
+            <Route path="/analyze" element={<AnalyzePage />} />
+            <Route path="/result" element={<DisplayResultPage />} />
+            <Route path="/" element={<LandingPageAnalyze />} />
+          </Routes>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </QueryClientProvider>
+
   );
 }
