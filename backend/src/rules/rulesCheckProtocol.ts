87bc208855812291ba67e547551f5caaa33eda05
Devjack404 <juansabarofek@gmail.com>
2026-09-23 15:41:48 +0700

    refactor: update checkProtocol function to return structured UrlProps instead of strings

--- backend/src/rules/rulesCheckProtocol.ts
+++ backend/src/rules/rulesCheckProtocol.ts
@@ -1,19 +1,44 @@
-export function checkProtocol(urlText : string): string { 
+// import { promises } from "node:dns";
+
+interface UrlProps {
+    status  : "Error" | "Warning" | "Aman";
+    message : string;
+    protocol : string;
+}
+
+export function checkProtocol(urlText : string): UrlProps { 
     try {
         const url = new URL(urlText);
 
         if (url.protocol === "http:") {
-            return "Peringatan : URL tidak menggunakan HTTPS";
-        }
+           return {
+            status : "Warning",
+            message : "URL tidak menggunakan HTTPS",
+            protocol : url.protocol,
+           }
+        } 
+        
 
         if (url.protocol === "https:") {
-            return "Aman : URL menggunakan HTTPS"
-        }
-        
-        return "Aman : URL menggunakan HTTPS";
-    }
+            return {
+                status : "Aman",
+                message : "URL menggunakan HTTPS",
+                protocol : url.protocol,
+            }
+        } 
 
+        return{
+            status :"Warning",
+            message : "URL tidak umum",
+            protocol : url.protocol
+        }
+    } 
+    
     catch {
-        return "Eror : Format URL tidak umum";
+        return{
+            status : "Error",
+            message : "URL tidak valid",
+            protocol : "Unknown"
+        };
     }
 }
